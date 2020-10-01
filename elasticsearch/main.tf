66b88745350e9b5261030272ed1407060f120629
Abhilash Bolla <abhilash.gis@gmail.com>
2020-10-01 22:20:23 +0530

    Added resources for Elasticsearch, ec2 instance and security groups

--- elasticsearch/main.tf
+++ elasticsearch/main.tf
@@ -3,6 +3,27 @@ provider "aws" {
   region  = "ap-south-1"
 }
 
+resource "random_id" "index" {
+  byte_length = 2
+}
+
 locals {
-  Environment = "Test"
+  Environment             = "Test"
+  subnet_ids_list         = var.public_subnet_ids
+  subnet_ids_random_index = random_id.index.dec % length(local.subnet_ids_list)
+}
+
+resource "aws_instance" "elasticsearch_instance" {
+  count                  = "${var.num_of_instances}"
+  ami                    = "ami-009110a2bf8d7dd0a"
+  instance_type          = "t3.micro"
+  subnet_id              = element(var.public_subnet_ids, local.subnet_ids_random_index)
+  vpc_security_group_ids = ["${aws_security_group.elasticsearch_security_group.id}"]
+  key_name               = var.key_pair
+
+  tags = {
+    Name        = "${local.Environment} Elasticsearch instance ${count.index}"
+    Environment = "${local.Environment}"
+    Creator     = "Terraform"
+  }
 }
---
+++ elasticsearch/security_groups.tf
@@ -0,0 +1,33 @@
+resource "aws_security_group" "elasticsearch_security_group" {
+  name   = "${terraform.workspace} Elasticsearch Security Group"
+  vpc_id = "${var.vpc_id}"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9200
+    to_port     = 9200
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9300
+    to_port     = 9300
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
