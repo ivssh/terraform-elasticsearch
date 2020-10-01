560a84719d96d86deff23999efaa49fae8b6a322
Abhilash Bolla <abhilash.gis@gmail.com>
2020-10-02 03:15:03 +0530

    Added a post runner to configure elasticsearch.yml file

--- elasticsearch/runners.tf
+++ elasticsearch/runners.tf
@@ -4,14 +4,14 @@ resource "null_resource" "elasticsearch-runner" {
   depends_on = [aws_eip.elasticsearch_eip]
 
   connection {
-    type = "ssh"
-    user = "ubuntu"
+    type        = "ssh"
+    user        = "ubuntu"
     private_key = "${file("~/.ssh/yulu_assignment.pem")}"
-    host = "${element(aws_eip.elasticsearch_eip.*.public_ip, count.index)}"
+    host        = "${element(aws_eip.elasticsearch_eip.*.public_ip, count.index)}"
   }
 
   provisioner "file" {
-    source = "elasticsearch/install_elasticsearch.sh"
+    source      = "elasticsearch/install_elasticsearch.sh"
     destination = "/home/ubuntu/install_elasticsearch.sh"
   }
 
@@ -19,7 +19,7 @@ resource "null_resource" "elasticsearch-runner" {
     # If no binary is specified, we'll copy /dev/null (always 0 bytes) to the
     # instance. The "remote-exec" block will then overwrite that. There's no
     # such thing as conditional file copying in Terraform, so we fake it.
-    source = "${coalesce(var.elastic_binary, "/dev/null")}"
+    source      = "${coalesce(var.elastic_binary, "/dev/null")}"
     destination = "/home/ubuntu/elasticsearch"
   }
 
@@ -27,6 +27,30 @@ resource "null_resource" "elasticsearch-runner" {
   provisioner "remote-exec" {
     inline = [
       "sudo bash install_elasticsearch.sh"
+    ]
+  }
+}
+
+resource "null_resource" "elasticsearch-post-runner" {
+  count = "${var.num_of_instances}"
+
+  depends_on = [aws_eip.elasticsearch_eip]
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = "${file("~/.ssh/yulu_assignment.pem")}"
+    host        = "${element(aws_eip.elasticsearch_eip.*.public_ip, count.index)}"
+  }
+
+  # Install Elasticsearch.
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'node.name: ${element(aws_instance.elasticsearch_instance.*.private_ip, count.index)}' | sudo tee -a /etc/elasticsearch/elasticsearch.yml",
+      "echo 'network.host : 0.0.0.0' | sudo tee -a /etc/elasticsearch/elasticsearch.yml",
+      "echo  'discovery.seed_hosts:  ${jsonencode(aws_instance.elasticsearch_instance.*.private_ip)}' | sudo tee -a /etc/elasticsearch/elasticsearch.yml",
+      "echo 'cluster.name: elasticsearch' | sudo tee -a /etc/elasticsearch/elasticsearch.yml",
+      "echo 'xpack.security.enabled: true' | sudo tee -a /etc/elasticsearch/elasticsearch.yml"
     ]
   }
 }
